dac193d3bf888e636b51077fd07ac8dd7f7024bf
KittikonDonde <kittikon1659@gmail.com>
2024-01-09 16:29:38 +0700

    add new

--- app/src/pages/Finance.jsx
+++ app/src/pages/Finance.jsx
@@ -10,42 +10,8 @@ function Finance() {
             <header class="site-header">
                 <div class="container">
                     <div class="row">
-                        <div class="col-lg-8 col-12 d-flex flex-wrap">
-                            <p class="d-flex me-4 mb-0">
-                                <i class="bi-geo-alt me-2"></i>
-                                Akershusstranda 20, 0150 Oslo, Norway
-                            </p>
-                            <p class="d-flex mb-0">
-                                <i class="bi-envelope me-2"></i>
-
-                                <a href="mailto:info@company.com">
-                                    info@company.com
-                                </a>
-                            </p>
-                        </div>
-                        <div class="col-lg-3 col-12 ms-auto d-lg-block d-none">
-                            <ul class="social-icon">
-                                <li class="social-icon-item">
-                                    <a href="#" class="social-icon-link bi-twitter"></a>
-                                </li>
-
-                                <li class="social-icon-item">
-                                    <a href="#" class="social-icon-link bi-facebook"></a>
-                                </li>
-
-                                <li class="social-icon-item">
-                                    <a href="#" class="social-icon-link bi-instagram"></a>
-                                </li>
-
-                                <li class="social-icon-item">
-                                    <a href="#" class="social-icon-link bi-youtube"></a>
-                                </li>
-
-                                <li class="social-icon-item">
-                                    <a href="#" class="social-icon-link bi-whatsapp"></a>
-                                </li>
-                            </ul>
-                        </div>
+                        
+                    
                     </div>
                 </div>
             </header>
@@ -75,7 +41,7 @@ function Finance() {
                 <section class="testimonial-section section-padding section-bg">
                     <div class="container">
                         <div class="row">
-                        <h2 class="mb-lg-3">จองห้องต่างๆ</h2>
+                        <h2 class="mb-lg-3">การเงิน</h2>
                         <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
                                 <div class="featured-block d-flex justify-content-center align-items-center">
                                     <a href="http://www.mhcc.moph.go.th/payroll/home1.php" class="d-block">
